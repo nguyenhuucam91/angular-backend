650ace15e3fa8bd4438c07b4d4761bd3c9ac0160
camnh <nguyenhuucam91@gmail.com>
2019-11-07 18:37:30 +0700

    adding categories

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller 
+{
+    public function index(Request $request)
+    {
+        $perPage = $request->query('perpage');
+        $categories = Category::paginate($perPage);
+        return response()->json(['data' => $categories]);
+    }
+
+    public function store()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'Api\AuthController@login');
     Route::post('logout', 'Api\AuthController@logout');
     Route::post('refresh', 'Api\AuthController@refresh');
     Route::post('me', 'Api\AuthController@me');
-
 });
+
+
+Route::resource('categories', 'Api\CategoryController')->middleware('api');
