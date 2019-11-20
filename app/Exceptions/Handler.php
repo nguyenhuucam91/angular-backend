5d4a6950279f38ef7152b7d247dea85f76ad1b71
MacOne <mac@MacOnes-MacBook-Pro.local>
2019-11-20 14:49:35 +0700

    push

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,9 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        // if ($exception instanceof ) {
-
-        // }
+        if ($request->expectsJson()) {
+            return response()->json(['success' => false, 'error' => $exception->getMessage()]);
+        }
         return parent::render($request, $exception);
     }
 }
